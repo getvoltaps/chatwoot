579efb88b4d3ed29686198bef1cafb5f262487d2
Jonas Jersborg <jonasjersborg@gmail.com>
2026-02-23 22:33:52 +0100

    Volt patch: create new conversation when reply is from different sender

--- app/services/mailbox/conversation_finder.rb
+++ app/services/mailbox/conversation_finder.rb
@@ -12,17 +12,27 @@ class Mailbox::ConversationFinder
   end
 
   def find
+    sender_email = @mail.from&.first&.downcase
+
     @strategies.each do |strategy_class|
       conversation = strategy_class.new(@mail).find
-
       next unless conversation
+
+      # VOLT PATCH: If this is not a new conversation strategy and the sender
+      # is different from the conversation's contact, create a new conversation instead.
+      if strategy_class != Mailbox::ConversationFinderStrategies::NewConversationStrategy
+        contact_email = conversation.contact&.email&.downcase
+        if contact_email && sender_email && contact_email != sender_email
+          Rails.logger.info "Volt patch: sender #{sender_email} differs from contact #{contact_email}, creating new conversation"
+          next
+        end
+      end
 
       strategy_name = strategy_class.name.demodulize.underscore
       Rails.logger.info "Conversation found via #{strategy_name} strategy"
       return conversation
     end
 
-    # Should not reach here if NewConversationStrategy is in the chain
     Rails.logger.error 'No conversation found via any strategy (NewConversationStrategy missing?)'
     nil
   end
